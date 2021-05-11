c0e7b317af9ee4dddbe8573a67a25a91ed6b79b1
ayman <aymanjaber2012@gmail.com>
2021-05-11 20:38:32 +0300

    Fix eslint errors

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
-const WEATHER_API_KEY = 'Insert_API_Key_Here'
+const WEATHER_API_KEY = 'Insert_API_KeyHere';
 // You can get an api key by subscribing to this website: https://openweathermap.org/
 
-let myLocation = window.prompt("Enter the location where you wish to get Weather information for")
-myLocation = myLocation.trim()
-if (myLocation == null || myLocation == '') myLocation = "London"
+let myLocation = window.prompt('Enter the location where you wish to get Weather information for');
+myLocation = myLocation.trim();
+if (myLocation == null || myLocation === '') myLocation = 'London';
 
+let currentLocation = '';
+let coordinates = [];
+let avgTemp = '';
+let pressure = '';
+let humidity = '';
+let description = '';
 
-let currentLocation = ''
-let coordinates = []
-let avgTemp = ''
-let pressure = ''
-let humidity = ''
-let description = ''
+const documentLocation = document.getElementById('location');
+const documentLongitude = document.getElementById('longitude');
+const documentLatitude = document.getElementById('latitude');
+const documentTemperature = document.getElementById('temperature');
+const documentPressure = document.getElementById('pressure');
+const documentHumidity = document.getElementById('humidity');
+const documentDescription = document.getElementById('description');
 
-let document_location = document.getElementById('location')
-let document_longitude = document.getElementById('longitude')
-let document_latitude = document.getElementById('latitude')
-let document_temperature = document.getElementById('temperature')
-let document_pressure = document.getElementById('pressure')
-let document_humidity = document.getElementById('humidity')
-let document_description = document.getElementById('description')
+const weatherData = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${myLocation}&appid=${WEATHER_API_KEY}`, {
+  mode: 'cors',
+}).then((response) => response.json()).then((data) => {
+  currentLocation = data.name;
+  coordinates = [data.coord.lon, data.coord.lat];
+  avgTemp = Number(data.main.temp) - 273;
+  pressure = data.main.pressure;
+  humidity = data.main.humidity;
+  description = data.weather[0].description;
 
-weatherData = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${myLocation}&appid=${WEATHER_API_KEY}`, {
-    mode: 'cors'
-}).then(response => response.json()).then(data => {
-    console.log(data)
-    
-    currentLocation = data.name
-    coordinates = [data.coord.lon,  data.coord.lat]
-    avgTemp = Number(data.main.temp) - 273
-    pressure = data.main.pressure
-    humidity = data.main.humidity
-    description = data.weather[0].description
-
-
-    document_location.innerHTML += currentLocation
-    document_latitude.innerHTML += coordinates[1]
-    document_longitude.innerHTML += coordinates[0]
-    document_temperature.innerHTML += avgTemp
-    document_pressure.innerHTML += pressure
-    document_humidity.innerHTML += humidity
-    document_description.innerHTML += description
-
-    
-
-})
-
+  documentLocation.innerHTML += currentLocation;
+  documentLatitude.innerHTML += coordinates[1];
+  documentLongitude.innerHTML += coordinates[0];
+  documentTemperature.innerHTML += avgTemp;
+  documentPressure.innerHTML += pressure;
+  documentHumidity.innerHTML += humidity;
+  documentDescription.innerHTML += description;
+});
